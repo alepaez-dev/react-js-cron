3b88c61aad4c621644a971dbac35784f83c7cf21
alepaez-dev <al3_hp@hotmail.com>
2022-01-12 21:33:25 -0600

    feat: react-js-cron

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Cron, { CronError } from "react-js-cron";
+import "./index.css";
+import "antd/dist/antd.css";
+import { InfoCircleOutlined } from '@ant-design/icons'
+import { Divider, Input } from "antd";
+import {
+  FRENCH_LOCALE,
+} from './constants'
 
-function App() {
+export default function App() {
+const defaultValue = '* * * * *'
+  const [value, setValue] = React.useState(defaultValue)
+
+  const [firstOption, setFirstOption] = React.useState(defaultValue)
+  const [secondOption, setSecondOption] = React.useState(null)
+  const [thirdOption, setThirdOption] = React.useState(null)
+  const [error, onError] = React.useState()
+
+
+  const options = React.useMemo( () => {
+    return {
+      firstOption,
+      secondOption,
+      thirdOption
+    }
+  }, [firstOption, secondOption, thirdOption])
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>cron validor Ale Paez</h1>
+
+      <div>
+      <p>Default value: {defaultValue}</p>
+      <p>Value: {value}</p>
+      <p>Error: {error ? error.description : 'undefined'}</p>
+
+      <Cron value={firstOption} setValue={setFirstOption} className='my-project-cron' clearButton={false} onError={onError}/>
+      <Cron value={secondOption} setValue={setSecondOption} className='my-project-cron' clearButton={false} onError={onError}/>
+      <Cron value={thirdOption} setValue={setThirdOption} className='my-project-cron' clearButton={false} onError={onError}/>
+      <button onClick={() => console.log("options", options)}>Hola</button>
+
+      <div>
+        <InfoCircleOutlined style={{ marginRight: 5 }} />
+        <span style={{ fontSize: 12 }}>
+          The first value will always be used as default value 
+        </span>
+      </div>
+    </div>
     </div>
   );
 }
-
-export default App;
